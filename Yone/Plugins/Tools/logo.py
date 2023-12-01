4bf2d64e5a0600fbabffd0191e01dd0911d0a0c6
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-01 10:13:34 +0530

    Update logo.py

--- Yone/Plugins/Tools/logo.py
+++ Yone/Plugins/Tools/logo.py
@@ -324,10 +324,84 @@ async def lego(event):
     except Exception as e:
         await event.reply(f"ᴇʀʀᴏʀ {e}, ʀᴇᴩᴏʀᴛ ᴛʜɪs ᴀᴛ @{SUPPORT_CHAT} ")
 
+
+@register(pattern="^/blogo ?(.*)")
+async def lego(event):
+    quew = event.pattern_match.group(1)
+    if event.sender_id != OWNER_ID and not quew:
+        await event.reply(
+            "`ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴄʀᴇᴀᴛᴇ ʟᴏɢᴏ ʙᴀʙᴇ!`\n`Example /blogo <am>`"
+        )
+        return
+    pesan = await event.reply("**ᴄʀᴇᴀᴛɪɴɢ ʏᴏᴜʀ ʀᴇǫᴜᴇsᴛᴇᴅ ʟᴏɢᴏ ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ᴀ sᴇᴄ​...**")
+    try:
+        text = event.pattern_match.group(1)
+        randc = random.choice(LOGO_LINKS)
+        img = Image.open(io.BytesIO(requests.get(randc).content))
+        draw = ImageDraw.Draw(img)
+        image_widthz, image_heightz = img.size
+        fnt = glob.glob("./Yone/resources/Am/*")
+        randf = random.choice(fnt)
+        font = ImageFont.truetype(randf, 120)
+        bbox = draw.textbbox((0, 0), text, font=font)
+        w, h = bbox[2] - bbox[0], bbox[3] - bbox[1]
+        h += int(h * 0.21)
+        draw.text(
+            ((image_widthz - w) / 2, (image_heightz - h) / 2),
+            text,
+            font=font,
+            fill=(255, 255, 255),
+        )
+        x = (image_widthz - w) / 2
+        y = (image_heightz - h) / 2 + 6
+        draw.text(
+            (x, y),
+            text,
+            font=font,
+            fill="white",
+            stroke_width=1,
+            stroke_fill="black",
+        )
+        image_width, image_height = img.size
+        draw.text(
+            ((image_widthz - w) / 2, (image_heightz - h) / 2),
+            text,
+            font=font,
+            fill=(255, 255, 255),
+        )
+        x = (image_widthz - w) / 2
+        y = (image_heightz - h) / 2 + 6
+        draw.text(
+            (x, y),
+            text,
+            font=font,
+            fill="white",
+            stroke_width=1,
+            stroke_fill="black",
+        )
+        fname = "mukesh.png"
+        img.save(fname, "png")
+        await telethn.send_file(
+            event.chat_id,
+            file=fname,
+            caption=f"""━━━━━━━{BOT_NAME}━━━━━━━
+
+☘️ ʟᴏɢᴏ ᴄʀᴇᴀᴛᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ☘️
+◈──────────────◈
+🔥 ᴄʀᴇᴀᴛᴇᴅ ʙʏ : @{BOT_USERNAME}
+━━━━━━━{BOT_NAME}━━━━━━━""", buttons=button_row
+        )
+        await pesan.delete()
+        if os.path.exists(fname):
+            os.remove(fname)
+    except Exception as e:
+        await event.reply(f"ᴇʀʀᴏʀ {e}, ʀᴇᴩᴏʀᴛ ᴛʜɪs ᴀᴛ @{SUPPORT_CHAT} ")
+
 __mod_name__ = "Lᴏɢᴏ"
 
 __help__ = f"""
 @{BOT_USERNAME} ᴄᴀɴ ᴄʀᴇᴀᴛᴇ sᴏᴍᴇ ʙᴇᴀᴜᴛɪғᴜʟ ᴀɴᴅ ᴀᴛᴛʀᴀᴄᴛɪᴠᴇ ʟᴏɢᴏ ғᴏʀ ʏᴏᴜʀ ᴘʀᴏғɪʟᴇ ᴘɪᴄs.
 
 ❍ /logo (Text) *:* ᴄʀᴇᴀᴛᴇ ᴀ ʟᴏɢᴏ ᴏғ ʏᴏᴜʀ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴡɪᴛʜ ʀᴀɴᴅᴏᴍ ᴠɪᴇᴡ.
+❍ /blogo (Text) *:* ᴄʀᴇᴀᴛᴇ ᴀ ʟᴏɢᴏ ᴏғ ʏᴏᴜʀ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴡɪᴛʜ ʀᴀɴᴅᴏᴍ ᴠɪᴇᴡ.
 """
