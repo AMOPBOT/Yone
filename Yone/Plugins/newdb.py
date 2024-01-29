07338230e9c7b98cb4f26e9da0cbc2e6b93601bc
AMOPBOT <134328176+AMOPBOT@users.noreply.github.com>
2024-01-29 10:19:42 +0530

    Update newdb.py

--- Yone/Plugins/newdb.py
+++ Yone/Plugins/newdb.py
@@ -253,5 +253,5 @@ dispatcher.add_handler(USER_HANDLER, USERS_GROUP)
 dispatcher.add_handler(CHATLIST_HANDLER)
 dispatcher.add_handler(CHAT_CHECKER_HANDLER, CHAT_GROUP)
 
-__mod_name__ = "dev"
+__mod_name__ = "Am"
 __handlers__ = [(USER_HANDLER, USERS_GROUP), CHATLIST_HANDLER]
