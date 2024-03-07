bf4729ed1cc3933db7f62bf4b43521846246bc59
AMBOTYTOP <134328176+AMBOTYTOP@users.noreply.github.com>
2024-03-07 21:03:15 +0530

    Update kora.py

--- kora.py
+++ kora.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("cd && rm -rf Yone && git clone https://github.com/AMOPBOT/Yone && cd Yone && python3 -m Yone")
+system ("cd && rm -rf Yone && git clone https://github.com/AMBOTYTOP/Yone && cd Yone && python3 -m Yone")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
